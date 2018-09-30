d3db4c6bb2ec13b27b4331b21a3a75715b7cb882
ayoubga <42929812+ayoubga@users.noreply.github.com>
2018-09-30 12:25:09 +0200

    Update bot.1.js

--- bot.1.js
+++ bot.1.js
@@ -46,8 +46,7 @@ const Discord = require('discord.js');
     
     client.on("message", async message => {
         if(!message.channel.guild) return;
- var prefix= "!";
-        if(message.content.startsWith(prefix + 'server')) {
+         if(message.content.startsWith(prefix + 'server')) {
         let guild = message.guild
         let channel = message.channel
         let guildicon = guild.icon_url
