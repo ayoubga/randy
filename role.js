c51dfe380a4d17760d692ac7255cac6dba86990a
ayoubga <42929812+ayoubga@users.noreply.github.com>
2018-09-30 15:27:46 +0200

    Update role.js

--- role.js
+++ role.js
@@ -1,6 +1,6 @@
-const Discord = require('discord.js');
+ const Discord = require('discord.js');
     const client = new Discord.Client();
-    const prefix = '!'
+    const prefix = '+'
 
     client.on('ready', () => {
       console.log(`Logged in as ${client.user.tag}!`);
@@ -25,64 +25,11 @@ const Discord = require('discord.js');
       console.log('')
       console.log('╚[════════════════════════════════════]╝')
     });
-    client.on('ready', function(){
-        var ms = 10000 ;
-        var setGame = [' !invite ','FINEX SERVER | FX ','BY : بايثور ','FINEX BOT','(= Hi'];
-        var i = -1;
-        var j = 0;
-        setInterval(function (){
-            if( i == -1 ){
-                j = 1;
-            }
-            if( i == (setGame.length)-1 ){
-                j = -1;
-            }
-            i = i+j;
-            client.user.setGame(setGame[i],`https://www.twitch.tv/pythorxxx`);
-        }, ms);
-
-    })const Discord = require('discord.js');
-    const client = new Discord.Client();
-    const prefix = '!'
-
-    client.on('ready', () => {
-      console.log(`Logged in as ${client.user.tag}!`);
-    });
-    client.on('message', msg => {
-      if (msg.content === 'ping') {
-        msg.reply('Pong!');
-      }
-    });
-    client.on('ready', () => {
-      console.log('╔[══════════════════════════════════]╗');
-      console.log('')
-      console.log('            ╔[════════════]╗')
-      console.log('              Bot Is Online')
-      console.log('            ╚[════════════]╝')
-      console.log('')
-      console.log(`Logged in as ${client.user.tag}!`);
-      console.log('')
-      console.log(`servers! [ " ${client.guilds.size} " ]`);
-      console.log('')
-      console.log(`Users! [ " ${client.users.size} " ]`);
-      console.log('')
-      console.log('╚[════════════════════════════════════]╝')
-    });
-    client.on('ready', function(){
-        var ms = 10000 ;
-        var setGame = [' !invite ','FINEX SERVER | FX ','BY : بايثور ','FINEX BOT','(= Hi'];
-        var i = -1;
-        var j = 0;
-        setInterval(function (){
-            if( i == -1 ){
-                j = 1;
-            }
-            if( i == (setGame.length)-1 ){
-                j = -1;
-            }
-            i = i+j;
-            client.user.setGame(setGame[i],`https://www.twitch.tv/pythorxxx`);
-        }, ms);
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+  client.user.setGame(` sky shope.`,'https://www.twitch.tv/v5bz');
+ }, ms);
 
     })
 
@@ -94,6 +41,5 @@ const Discord = require('discord.js');
 
 
 
-
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TKEN);
 
