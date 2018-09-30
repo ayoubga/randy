890ac849cf05d6e3c7e5e41536502e7fe9c19077
ayoubga <42929812+ayoubga@users.noreply.github.com>
2018-09-30 12:55:01 +0200

    Update music.js

--- music.js
+++ music.js
@@ -492,4 +492,4 @@ if (message.content.startsWith(PREFIX + 'setavatar')) {
    message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
 }
 });
-client.login(process.env.BOT_TOKEN);
+client.login(`NDk1OTEwNDY2ODAzOTkwNTYz.DpI8TA.qgGX5J32Fsv9ipMnKx39TTymgmo`);
