5f4fcc07c76f2b5834106abc9f4aa1fd3794f514
ayoubga <42929812+ayoubga@users.noreply.github.com>
2018-09-30 15:39:25 +0200

    Update role.js

--- role.js
+++ role.js
@@ -12,21 +12,11 @@ client.on('ready', () => {
   console.log('---------------')
 });
 
- client.on('message', message => {
-                                if(!message.channel.guild) return;
-                        if (message.content.startsWith('*ping')) {
-                            if(!message.channel.guild) return;
-                            var msg = `${Date.now() - message.createdTimestamp}`
-                            var api = `${Math.round(client.ping)}`
-                            if (message.author.bot) return;
-                        let embed = new Discord.RichEmbed()
-                        .setAuthor(message.author.username,message.author.avatarURL)
-                        .setColor('RANDOM')
-                        .addField('**Time Taken:**',msg + " ms :signal_strength: ")
-                        .addField('**WebSocket:**',api + " ms :signal_strength: ")
-         message.channel.send({embed:embed});
-                        }
-                    });
+client.on('message', msg => {
+      if (msg.content === 'ping') {
+        msg.reply('Pong!');
+      }
+    });
 
 
 
