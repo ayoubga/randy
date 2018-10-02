38e907bb71f76f2ce9adfc0801050109145e27b0
ayoubga <42929812+ayoubga@users.noreply.github.com>
2018-10-02 14:04:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,11 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', () => {
+  client.user.setGame('Sky Shop','https://www.twitch.tv/peery13');
+  console.log('---------------');
+  console.log(' Bot Is Online')
+  console.log('---------------')
+});
+const prefix = '-'
 
+client.login(process.env.BOT_TOKEN);
