441650be00c822e898104887ebe8ad33cc1b1311
ayoubga <42929812+ayoubga@users.noreply.github.com>
2018-10-04 13:40:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -362,6 +362,61 @@ client.on('voiceStateUpdate', (old, now) => {
   if (!size) return channel.setName(`Voice Online: ${currentSize}`);
   if (currentSize !== size) channel.setName(`Voice Online: ${currentSize}`);
 });
+////////////////////////////////////////////////////
+client.on('ready', () = {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message = {
+            if (message.content === 'السلام عليكم') {
+              message.channel.send(' وعليكم السلام و رحمة الله و بركاته');
+              message.channel.sendFile(.Photo.PNG);
+               
+
+            }
+});
+////////////////////////////////////////////////////////////////
+
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "Sky Shop ,")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        })
+    })
+})
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.find('name', 'sky');
+    if (!channel) {
+        console.log("!channel fails");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('made it till here!');
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "Sky Shop ,")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+                    console.log(3);
+                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
+ channel.send(` ♥ **تم دعوته من قبل ${Invite.inviter} ♥ `)            
+ }
+            dat[Inv] = Invite.uses;
+        })
+    })
+});
 
 
 client.login(process.env.BOT_TOKEN);
