66b262e4ac3a675244814f3d2ec88dd07936be95
ayoubga <42929812+ayoubga@users.noreply.github.com>
2018-10-04 13:19:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -273,7 +273,46 @@ client.on('message', msg => {
     }
 }
 });
+///////////////////////////////////////////////////////////////////
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("487670104646090754");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        });
+    });
+});
 
+
+
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("487670105279299626");
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("487670104646090754");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;         
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
 
 
 
