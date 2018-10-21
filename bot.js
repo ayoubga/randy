4152ef866864767a66e718da667e4493d838b00f
ayoubga <42929812+ayoubga@users.noreply.github.com>
2018-10-21 13:45:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,6 +35,48 @@ client.user.setGame(`Sky Shop`,"http://twitch.tv/S-F")
   
 
 /////////////////////////////////////////////////
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("487670104646090754 ");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        });
+    });
+});
+ 
+ 
+ 
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("487670105279299626");
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("487670104646090754");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+                    setTimeout(function() {
+ channel.send(`**invited by** ${Invite.inviter} `) ;
+                    },1500);
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
+///////////////////////////////////////////////////////////////////
 
 client.on('message', async message =>{
   if (message.author.boss) return;
