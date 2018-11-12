8f91db55f236a06e275242a32b5431c0b0d2ef31
ayoubga <42929812+ayoubga@users.noreply.github.com>
2018-11-12 13:09:32 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -643,5 +643,17 @@ if (message.content === '!support') {
     }
    } 
   });
+client.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('!bcu')){
+ if (message.author.id !== '399353508429824000') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+ if(!message.author.id === '399353508429824000') return;
+message.channel.sendMessage('جار ارسال الرسالة |✅')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
 client.login(process.env.BOT_TOKEN);
 
